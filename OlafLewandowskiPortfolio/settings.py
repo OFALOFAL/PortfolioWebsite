a265c9473500d5d14af2e036392fee9e6ef01119
Olaf 3 <91374689+OFALOFAL@users.noreply.github.com>
2023-09-26 21:27:46 +0200

    update settings.py

--- OlafLewandowskiPortfolio/settings.py
+++ OlafLewandowskiPortfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'OlafLewandowskiPortfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -114,11 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = './static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "Website/static/",
+    os.path.join(BASE_DIR, "Website/static/"),
 ]
-STATIC_ROOT = BASE_DIR / "Website/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "Website/static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
